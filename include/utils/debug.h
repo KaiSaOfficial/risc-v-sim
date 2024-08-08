0ceb952d45aef2d994c86771fd41758da2dcf065
KaiSaOfficial <1730375515@qq.com>
2024-08-08 20:16:58 +0800

    add debug LOG

---
+++ include/utils/debug.h
@@ -0,0 +1,18 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include "utils.h"
+
+#define Error(format, ...)                                                     \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_RED) "\n", __FILE__, __LINE__, \
+         __func__, ##__VA_ARGS__)
+
+#define Log(format, ...)                                               \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_GREEN) "\n", __FILE__, \
+         __LINE__, __func__, ##__VA_ARGS__)
+
+#define Debug(format, ...)                                                     \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_RED) "\n", __FILE__, __LINE__, \
+         __func__, ##__VA_ARGS__)
+
+#endif //__DEBUG_H__
